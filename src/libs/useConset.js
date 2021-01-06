bbad821a0e809cdcef7128ef368e80309c636422
zhangjianlei <djkloopjs@gmail.com>
2021-01-06 15:03:03 +0800

    增加对row && col
    增加对row && col 的控制

---
+++ src/libs/useConset.js
@@ -0,0 +1,14 @@
+/*
+ * @Author        : djkloop
+ * @Date          : 2021-01-06 14:52:05
+ * @LastEditors   : djkloop
+ * @LastEditTime  : 2021-01-06 14:54:42
+ * @Description   : 头部注释
+ * @FilePath      : /test_fc/src/libs/useConset.js
+ */
+export const CODE = {
+  ERR_1: {
+    CODE: 1000,
+    TITLE: '配置错误'
+  }
+}
--- src/packages/store/index.js
+++ src/packages/store/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author        : djkloop
  * @Date          : 2020-12-31 18:19:47
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-01-05 16:49:15
+ * @LastEditors   : djkloop
+ * @LastEditTime  : 2021-01-06 15:01:52
  * @Description   : 这个文件有很多东西是为了学习存在的...例如rxjs
  * @FilePath      : /test_fc/src/packages/store/index.js
  */
@@ -11,6 +11,7 @@ import { Notification } from 'element-ui'
 import { cloneDeep } from 'lodash'
 import { BehaviorSubject } from 'rxjs'
 import { useAutoField } from '@/libs/useUtils'
+import { CODE } from '@/libs/useConset'
 import * as dot from 'dot-wild';
 
 
@@ -55,8 +56,6 @@ Mediator.prototype.CreateObserverItem = function(configInstance) {
   const { type, field, name, originItem } = configInstance
   let cloneConfigJsonArray = cloneDeep(this.configJson[type]) || []
   const _field = field || name
-  console.log(originItem);
-  
   if (Array.isArray(cloneConfigJsonArray) && cloneConfigJsonArray.length) {
     cloneConfigJsonArray.forEach(item => {
       item['field'] = useAutoField() /// 每个item生成唯一的key
@@ -67,20 +66,34 @@ Mediator.prototype.CreateObserverItem = function(configInstance) {
       }
     })
     let childrenConfigArray = []
+    /// 如果是布局组件 需要把底部的col的属性也拿出来拍平给右边设置
     if (originItem.type === 'el-row') {
-      childrenConfigArray = originItem.children.map((item, idx) => {
-        const _field = item.name
-        item['field'] = useAutoField() /// 每个item生成唯一的key
-        item['target_field'] = _field /// 每个item要对应当前的item key    
-        if(dot.has(originItem.children[idx], item.target)) {
-          const val = dot.get(originItem.children[idx], item.target)
-          item['value'] = val
+      childrenConfigArray = originItem.children.map((childrenItem, idx) => {
+        /// 现先获取当前组件的所有子组件
+        /// 然后根据每个子组件的类型获取对应的type-json
+        /// 然后在循环每个type-json数组取匹配target然后给对应的item赋值
+        /// 最后返回出当前的type-json合并到总的数据里面去拍平
+        const chidlrenCloneConfigJsonArray = cloneDeep(this.configJson[childrenItem.type]) || []
+        if (Array.isArray(chidlrenCloneConfigJsonArray) && chidlrenCloneConfigJsonArray.length) {
+          chidlrenCloneConfigJsonArray.forEach(chidlrenCloneConfigItem => {
+            chidlrenCloneConfigItem['field'] = useAutoField()
+            /// el-col组件因为是布局组件没有field，所有它的tagrt_filed是组件的name
+            chidlrenCloneConfigItem['target_field'] = childrenItem.name
+            if (dot.has(childrenItem, chidlrenCloneConfigItem.target)) {
+              const val = dot.get(childrenItem, chidlrenCloneConfigItem.target)
+              chidlrenCloneConfigItem['value'] = val
+            }
+          })
+          return chidlrenCloneConfigJsonArray
+        } else {
+          Notification({
+            title: `错误code: ${CODE.ERR_1.code}`,
+            message: `当前第${idx + 1}子组件没有对应的属性配置JSON！为了保证代码运行，请添加对应的配置属性！`
+          })
         }
-        return item
       })
     }
-    cloneConfigJsonArray = [...cloneConfigJsonArray, ...childrenConfigArray]
-    console.log(cloneConfigJsonArray);
+    cloneConfigJsonArray = [...cloneConfigJsonArray, ...childrenConfigArray].flat(Infinity)
   } else {
     Notification.error("当前组件没有对应的属性配置JSON!")
   }
